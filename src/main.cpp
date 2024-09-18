c0bed7046f4a844da36b2b2f20ccecb7050a6f92
John Jones <johnjones@Johns-MBP-2.house.johnjonesfour.com>
2024-09-18 19:43:35 -0400

    added motion sensor

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,18 @@
 #include <Adafruit_VS1053.h>
 #include <SD.h>
 
-// These are the pins used for the breakout example
-#define BREAKOUT_RESET  9      // VS1053 reset pin (output)
-#define BREAKOUT_CS     10     // VS1053 chip select pin (output)
-#define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
-// These are the pins used for the music maker shield
 #define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
 #define SHIELD_CS     7      // VS1053 chip select pin (output)
 #define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
-
-// These are common pins between breakout and shield
 #define CARDCS 4     // Card chip select pin
-// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
 #define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
 
 Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
 
+#define PIR_MOTION_SENSOR 11
+#define DEBOUNCE 10000
+
+unsigned long started = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -37,10 +33,12 @@ void setup() {
   }
   Serial.println("SD OK!");
 
-  Serial.println(F("Playing full track 001"));
-  musicPlayer.playFullFile("/laugh.wav");
+  pinMode(PIR_MOTION_SENSOR, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (digitalRead(PIR_MOTION_SENSOR) && millis() - started >= DEBOUNCE) {
+    started = millis();
+    musicPlayer.startPlayingFile("/laugh.wav");
+  }
 }
