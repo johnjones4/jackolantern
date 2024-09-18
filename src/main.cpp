a96bff1c7d983b131eb85225af105acf3f8b5e95
John Jones <johnjones@Johns-MacBook-Pro-2.local>
2024-09-18 19:27:54 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
+
+#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
+#define VS1053_CS       6     // VS1053 chip select pin (output)
+#define VS1053_DCS     10     // VS1053 Data/command select pin (output)
+#define CARDCS          5     // Card chip select pin
+// DREQ should be an Int pin *if possible* (not possible on 32u4)
+#define VS1053_DREQ     9     // VS1053 Data request, ideally an Interrupt pin
+
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting up");
+
+  if (! musicPlayer.begin()) { // initialise the music player
+    Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+    while (1);
+  }
+
+  Serial.println(F("VS1053 found"));
+
+  if (!SD.begin(CARDCS)) {
+    Serial.println(F("SD failed, or not present"));
+    while (1);  // don't do anything more
+  }
+  Serial.println("SD OK!");
+
+  Serial.println(F("Playing full track 001"));
+  musicPlayer.playFullFile("/laugh.wav");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
